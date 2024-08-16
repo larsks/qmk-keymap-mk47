0fb0ed2bc5dc44f7314bf41b3afd160199eedd7f
Lars Kellogg-Stedman <lars@oddbit.com>
2024-08-15 23:08:35 -0400

    Keymap for Inland MK47

---
+++ rules.mk
@@ -0,0 +1,16 @@
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+
+# From https://docs.qmk.fm/squeezing_avr
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+# https://docs.qmk.fm/features/grave_esc
+GRAVE_ESC_ENABLE = no
+
+# Enabling VIA support appears to break our keymap.
+VIA_ENABLE = no
