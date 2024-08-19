8ffb5ac3cd5ce08029d330aa124974ebbbefcea2
Lars Kellogg-Stedman <lars@oddbit.com>
2024-08-18 22:56:49 -0400

    Make slash key LSFT_T(KC_SLASH)

--- keymap.c
+++ keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_BASE] = LAYOUT_planck_mit(
         TD(TD_ESC_GRV), KC_Q,  KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,         KC_A,  KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT,        KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
+        KC_LSFT,        KC_Z,  KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_UP,   LSFT_T(KC_SLSH),
         KC_LCTL,        MO(2), KC_LALT, KC_LGUI, KC_PGDN, KC_SPC,          KC_PGUP, MO(1), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
